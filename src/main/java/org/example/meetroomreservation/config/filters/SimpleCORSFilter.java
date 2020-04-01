51a6c9430586d109d2fbc28c659b70addc54e3d0
AnnaTulisova <a.tulisova@mail.ru>
2020-04-01 17:54:20 +0400

    Added first changes for REST API technologies

---
+++ src/main/java/org/example/meetroomreservation/config/filters/SimpleCORSFilter.java
@@ -0,0 +1,13 @@
+package org.example.meetroomreservation.config.filters;
+
+import org.springframework.stereotype.Component;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Component
+public class SimpleCORSFilter implements WebMvcConfigurer {
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**");
+    }
+}
--- src/main/java/org/example/meetroomreservation/controller/HomeController.java
+++ src/main/java/org/example/meetroomreservation/controller/HomeController.java
@@ -6,19 +6,24 @@ import org.example.meetroomreservation.service.ReservationService;
 import org.example.meetroomreservation.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 import java.util.Map;
 
-@Controller
+@CrossOrigin(origins = "http://localhost:4200")
+@RestController
 public class HomeController {
     @Autowired
     private UserService userService;
 
     @Autowired
     private ReservationService reservationService;
+
+    @RequestMapping(value = "/users", method = RequestMethod.GET, produces = "application/json")
+    public List<User> home() {
+        return  userService.findAll();
+    }
 
     @GetMapping
     public  String home(Map<String, Object> model){
