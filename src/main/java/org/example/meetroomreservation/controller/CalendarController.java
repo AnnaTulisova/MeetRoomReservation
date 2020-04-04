89dd6cb7ae947f6ba9b4de1b00c42007442ca8a6
AnnaTulisova <a.tulisova@mail.ru>
2020-04-04 23:24:22 +0400

    Updated Calendar Controller

--- src/main/java/org/example/meetroomreservation/controller/CalendarController.java
+++ src/main/java/org/example/meetroomreservation/controller/CalendarController.java
@@ -1,13 +1,14 @@
 package org.example.meetroomreservation.controller;
 
-import org.example.meetroomreservation.domain.viewModels.ReservationViewModel;
+import org.example.meetroomreservation.domain.viewModels.ReservationView;
+import org.example.meetroomreservation.domain.requestModels.*;
 import org.example.meetroomreservation.service.ReservationService;
 import org.example.meetroomreservation.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
-import java.util.Map;
 
 @RestController
 public class CalendarController {
@@ -19,53 +20,41 @@ public class CalendarController {
     private UserService userService;
 
     @RequestMapping(value = "/calendar", method = RequestMethod.GET, produces = "application/json")
-    public List<ReservationViewModel> calendar() {
-        List<ReservationViewModel> reservations = reservationService.findReservationsWithUsers();
+    public List<ReservationView> calendar() {
+        List<ReservationView> reservations = reservationService.findReservationsWithUsers();
         return reservations;
     }
 
-    @GetMapping("/calendar/reservation")
-    public String reservationInfo(@RequestParam String datetime, @RequestParam Integer meetroomId, Map<String, Object> model) {
-        ReservationViewModel reservation = reservationService.findReservationsWithUsers(datetime, meetroomId);
-
-        model.put("reservation", reservation);
-        model.put("meetroomId", meetroomId);
-        model.put("datetime", datetime);
-        return "reservationInfo";
+    @RequestMapping(value = "/calendar/reservation", method = RequestMethod.GET, produces = "application/json")
+    public ReservationView reservationInfo(@RequestBody ReservationRequest reservationRequest) {
+        ReservationView reservation = reservationService.findReservationsWithUsers(
+                reservationRequest.getDatetime(),
+                reservationRequest.getMeetroomId());
+        return reservation;
     }
 
-    @GetMapping("/add")
-    public String add(Map<String, Object> model){
-        return "add";
-    }
-
-    @PostMapping("/add")
-    public String add(@RequestParam String userId, @RequestParam Integer meetroomId, @RequestParam String datetime,
-                                 @RequestParam String duration) {
-        reservationService.addReservation(userId,  meetroomId, datetime, duration);
-        return "redirect:/calendar";
+    @PostMapping("/calendar/create")
+    public ResponseEntity<String> add(@RequestBody ReservationAddEdit reservationAdd) {
+        reservationService.addReservation(reservationAdd);
+        return ResponseEntity.ok("Added");
     }
 
     @PostMapping("/calendar/reservation/delete")
-    public String delete(@RequestParam String datetime, @RequestParam Integer meetroomId) {
-        reservationService.deleteByDatetimeAndMeetroomId(datetime, meetroomId);
-        return "redirect:/calendar";
+    public ResponseEntity<String> delete(@RequestBody ReservationRequest reservationRequest) {
+        reservationService.deleteByDatetimeAndMeetroomId(
+                reservationRequest.getDatetime(), reservationRequest.getMeetroomId());
+        return ResponseEntity.ok("Deleted");
     }
 
     @GetMapping("/calendar/reservation/edit")
-    public String edit(@RequestParam String datetime, @RequestParam Integer meetroomId,Map<String, Object> model) {
-        ReservationViewModel reservationViewModel = reservationService.findReservationsWithUsers(datetime, meetroomId);
-        String userIds = userService.getUserIdsFromReservation(reservationViewModel);
-        model.put("reservation", reservationViewModel);
-        model.put("userIds", userIds);
-        model.put("meetroomId", reservationViewModel.getMeetroom().getId().toString());
-        return "edit";
+    public ResponseEntity<ReservationAddEdit> edit(@RequestBody ReservationRequest reservationRequest) {
+        ReservationAddEdit reservationToEdit = reservationService.getReservationForEdit(reservationRequest);
+        return ResponseEntity.ok(reservationToEdit);
     }
 
-    @PostMapping("/calendar/reservation/edit")
-    private String edit(@RequestParam String datetime, @RequestParam Integer meetroomId, @RequestParam String newDatetime,
-                        @RequestParam Integer newMeetroomId, @RequestParam String userIds, @RequestParam String duration) {
-        reservationService.editReservation(datetime, meetroomId, newDatetime, newMeetroomId, userIds, duration);
-        return "redirect:/calendar/reservation?meetroomId="+newMeetroomId+"&datetime="+newDatetime;
+    @PutMapping("/calendar/reservation/edit")
+    private ResponseEntity<Void> edit(@RequestBody ReservationEdit reservationEdit) {
+        reservationService.editReservation(reservationEdit);
+        return ResponseEntity.noContent().build();
     }
 }
